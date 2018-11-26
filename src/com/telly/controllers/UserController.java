66015b5d88861c1fa438b49d773fbee3f08d67ca
islacine <fy_mestoui@esi.dz>
2018-11-26 19:20:44 +0100

    Gestion des reservations done par dev2

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -21,6 +21,43 @@ import com.telly.service.UserService;
 
 @Controller
 public class UserController {
+
+    @Autowired
+	ReserveService reserveService;
+
+    @RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+		
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		reserveService.reserve(reserve);
+	
+		
+		return "home";
+
+	}
+	
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+		
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+	
+		
+		return "home";
+
+	}
+	
 	
 }
 
