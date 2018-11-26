316a7de84eea3cebe542fadab6d30fc612df1db3
guedouarimohamed <mohamadovski999@gmail.com>
2018-11-26 12:29:30 +0100

    securite

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -21,6 +21,16 @@ import com.telly.service.UserService;
 
 @Controller
 public class UserController {
+
+    @RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 	
 }
 
